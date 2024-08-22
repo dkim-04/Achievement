8efca250588938a7906ad5146828f00bebb26f87
dkim-04 <dkim04@naver.com>
2024-08-22 10:37:27 +0900

    Update 파일 이름 바꾸기.py

--- python_응용/def_code/change_name/파일 이름 바꾸기.py
+++ python_응용/def_code/change_name/파일 이름 바꾸기.py
@@ -3,38 +3,31 @@ import pandas as pd
 import shutil
 import logging
 logging.debug("info log")
-folder_path='C:/Users/kim/P_Ver1/P_2/data'
+folder_path='C:/Users/kim/P_Ver1/P_2/data' # 탐색할 폴더의 경로
 search_phrase="checked_files_"
 
 def change_name():
+    # 루트 경로로 부터 하위 디렉토리 및 파일들 탐색
     for root, dirs, files in os.walk(folder_path):
         for file in files:
+            #파일 형식이 csv이면, 
             if file.endswith('.csv'):
-                old_file_path=os.path.join(root,file)
-                new_file_name=f"checked_files_{file}"
-                new_file_path= os.path.join(root,new_file_name) 
-                os.rename(old_file_path,new_file_path)
+                old_file_path=os.path.join(root,file) #현재 파일의 기존 경로 생성
+                new_file_name=f"checked_files_{file}" # 새로운 파일 이름 생성할 때 기존파일 앞에 'checked_files_' 추가
+                new_file_path= os.path.join(root,new_file_name)  #새 파일 경로 생성
+                os.rename(old_file_path,new_file_path)  #기존 파일을 새 파일 이름으로 변경
                 print(f"{new_file_name},,,,,,,{new_file_path}")
 
-# def find_files_phrase(folder_path,phrase):
-#     piles= os.listdir(folder_path)
-#     matching_files=[]
-#     for pile in piles:
-#         if phrase in pile:
-#             matching_files.append(pile) 
-#     return matching_files
-# matching_files=find_files_phrase(folder_path,phrase)
 
-# if matching_files:
-#     for pile in matching_files:
+
 found=True
 def check_word_in_file(file_path,phrase):
-    for root, dir,files in os.walk(file_path):
-        for pile in files:
-            if not phrase in pile:
+    for root, dir,files in os.walk(file_path): # 만약 file_path 디렉토리를 시작으로 하위 디렉토리까지 순회 했을 때
+        for pile in files:  
+            if not phrase in pile: #pile 안에 phrase가 없을 시 이름을 바꿈
                 change_name() 
-                break
-        if found:
+                break #반복문 끝내기
+        if found:  # 안에 phrase가 있을시 반복문 끝내기
             break
             
        
