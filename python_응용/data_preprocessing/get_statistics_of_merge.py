489caeee392d696512855ce619a6d85d0410ae1a
dkim-04 <dkim04@naver.com>
2024-08-22 14:09:33 +0900

    Update get_statistics_of_merge.py

--- python_응용/data_preprocessing/get_statistics_of_merge.py
+++ python_응용/data_preprocessing/get_statistics_of_merge.py
@@ -3,12 +3,14 @@
 ###
 import pandas as pd
 import os
+
+# 각 열의 항들을 계산하는 코드
 def calculate_column(file_path):
-    df =pd.read_csv(file_path)
-    column_medians = df.median(numeric_only=True)
-    column_std = df.std(numeric_only=True)
-    column_means = df.mean(numeric_only=True)
-    stats_df = pd.DataFrame({
+    df =pd.read_csv(file_path)  # file_pathd의 csv파일을 읽는다. 
+    column_medians = df.median(numeric_only=True)  # 열의 중앙값 구하기
+    column_std = df.std(numeric_only=True)  # 열의 표준편차
+    column_means = df.mean(numeric_only=True) # 열의 평균
+    stats_df = pd.DataFrame({    # 이를 stats_df라는 데이터프레임으로 저장
         'list': column_means.index,
         '평균': column_means.values,
         '중앙값': column_medians.values,
@@ -16,7 +18,7 @@ def calculate_column(file_path):
     })
     return stats_df
 
-
+# 계산 후 데이터 프레임을  다른 이름의 csv로 저장  
 def save_to_csv(dataframe,output_file):
     dataframe.to_csv(output_file,index=False)
 
